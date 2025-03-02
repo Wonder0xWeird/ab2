ae96c9ba5b71760a7ec4dff2a877b328d46e3c26
Lars Crawford <crawford.lars@gmail.com>
2025-03-02 17:14:29 +0000

    Fix: Remove references to non-existent components

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,2 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as RootLayout } from './RootLayout'; 
+// This file intentionally left empty.
+// Components will be added as they are implemented. 
